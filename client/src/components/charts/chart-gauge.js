503d178e5c388c8282d36807805a1c08b19ff9b5
Timur Olur <timurolur@gmail.com>
2024-06-20 08:16:09 +0300

    htmx2 release version, idiomorph extension support for gauge web component.

--- client/src/components/charts/chart-gauge.js
+++ client/src/components/charts/chart-gauge.js
@@ -1,60 +1,41 @@
 import { GaugeChart, GaugeTypes } from "@carbon/charts";
+import styles from "@carbon/charts/styles.min.css";
 
 /**
- * @typedef {"value" | "delta"  | "height" | "width" | "theme" | "status" | "color"} AttributeKeys
+ * @typedef {"value" | "height" | "width" | "theme" | "class" } AttributeKeys
  */
 
 export default class ChartGauge extends HTMLElement {
   /** @type {GaugeChart} */
-  #chart_inst;
-
-  /** @type {HTMLDivElement} */
-  #base_div;
+  #chart;
 
   /** @type {AttributeKeys[]} */
-  static observedAttributes = [
-    "value",
-    "delta",
-    "height",
-    "width",
-    "theme",
-    "status",
-    "color",
-  ];
+  static observedAttributes = ["value", "height", "width", "theme", "class"];
 
   constructor() {
     super();
   }
 
   connectedCallback() {
+    const shadow_dom = this.attachShadow({ mode: "closed" });
     const child = document.createElement("div");
-    this.#base_div = child;
-    this.replaceChildren(child);
+    const style = document.createElement("link");
+    style.rel = "stylesheet";
+    style.href = "/static/index.css";
 
-    const fill = window.getComputedStyle(this).fill;
     const value_text = this.getAttribute("value") ?? "0";
-    const delta_text = this.getAttribute("delta") ?? "0";
     const height = this.getAttribute("height") ?? undefined;
     const width = this.getAttribute("width") ?? undefined;
     const theme = this.getAttribute("theme") ?? "g90";
-    const color = this.getAttribute("color") ?? fill;
-    const status = this.getAttribute("status") ?? "success";
 
     let value_number = Number(value_text);
-    let delta_number = Number(delta_text);
-
     value_number = isNaN(value_number) ? 0 : value_number;
-    delta_number = isNaN(delta_number) ? 0 : delta_number;
 
-    this.#chart_inst = new GaugeChart(child, {
+    this.#chart = new GaugeChart(child, {
       data: [
         {
           group: "value",
           value: value_number,
-        },
-        {
-          group: "delta",
-          value: delta_number,
         },
       ],
       options: {
@@ -64,13 +45,10 @@ export default class ChartGauge extends HTMLElement {
         resizable: true,
         theme: theme,
         color: {
-          scale: {
-            value: color,
-          },
+          scale: { value: window.getComputedStyle(this).fill },
         },
         gauge: {
           type: GaugeTypes.SEMI,
-          status: status,
           showPercentageSymbol: true,
         },
         toolbar: {
@@ -78,66 +56,57 @@ export default class ChartGauge extends HTMLElement {
         },
       },
     });
+
+    shadow_dom.replaceChildren(child);
+    shadow_dom.prepend(style);
   }
 
   disconnectedCallback() {
-    this.#chart_inst?.destroy();
-    this.#base_div?.remove();
+    this.#chart?.destroy();
   }
 
   /**
    * @param {AttributeKeys} name
-   * @param {string} oldValue
-   * @param {string} newValue
+   * @param {string} old_value
+   * @param {string} new_value
    */
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (!this.#chart_inst) return;
-
-    /** @type {{group:string, value:number}[]} */
-    const data = this.#chart_inst.model.getData();
+  attributeChangedCallback(name, old_value, new_value) {
+    if (!this.#chart) return;
 
     /** @type {import('@carbon/charts').GaugeChartOptions} */
-    const options = this.#chart_inst.model.getOptions();
+    const options = this.#chart.model.getOptions();
 
     switch (name) {
-      case "value":
-        const point = data.find((e) => e.group === "value");
-        if (point) {
-          point.value = Number(newValue) ?? 0;
-        }
+      case "value": {
+        this.#chart.model.setData([
+          {
+            group: "value",
+            value: Number(new_value) ?? 0,
+          },
+        ]);
         break;
-      case "delta":
-        const delta = data.find((e) => e.group === "delta");
-        if (delta) {
-          delta.value = Number(newValue) ?? 0;
-        }
-        break;
+      }
       case "height":
-        options.height = newValue;
+        options.height = new_value;
         break;
       case "width":
-        options.width = newValue;
+        options.width = new_value;
         break;
       case "theme":
-        options.theme = newValue;
+        options.theme = new_value;
         break;
-      case "status":
-        if (options.gauge) {
-          options.gauge.status = newValue;
-        }
-        break;
-      case "color":
+      case "class": {
         options.color = {
+          ...options.color,
           scale: {
-            value: newValue,
+            value: window.getComputedStyle(this).fill,
           },
         };
-        break;
+      }
     }
 
-    this.#chart_inst.update(true);
+    this.#chart.update(true);
   }
 }
 
 customElements.define("chart-gauge", ChartGauge);
-
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,37 @@
  * Exports all web components, registers dom load events.
  **/
 
-import "htmx.org";
+import htmx from "htmx.org/dist/htmx.esm.js";
+import { Idiomorph } from "idiomorph/dist/idiomorph.esm.js";
 import { load_local_theme } from "./components/theme_selector.js";
 
 export * from "./components/charts/chart-gauge.js";
 export * from "./components/notification.js";
 export * from "./components/confirmation_modal.js";
 export * from "./components/theme_selector.js";
+
+function createMorphConfig(swapStyle) {
+  if (swapStyle === "morph" || swapStyle === "morph:outerHTML") {
+    return { morphStyle: "outerHTML" };
+  } else if (swapStyle === "morph:innerHTML") {
+    return { morphStyle: "innerHTML" };
+  } else if (swapStyle.startsWith("morph:")) {
+    return Function("return (" + swapStyle.slice(6) + ")")();
+  }
+}
+
+htmx.defineExtension("morph", {
+  isInlineSwap: function (swapStyle) {
+    let config = createMorphConfig(swapStyle);
+    return config.swapStyle === "outerHTML" || config.swapStyle == null;
+  },
+  handleSwap: function (swapStyle, target, fragment) {
+    let config = createMorphConfig(swapStyle);
+    if (config) {
+      return Idiomorph.morph(target, fragment.children, config);
+    }
+  },
+});
 
 window.addEventListener(
   "DOMContentLoaded",
