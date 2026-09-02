6d10d51eb89c56c60cc7eb43aa2ca71f1c2010fe
Timur Olur <timurolur@gmail.com>
2026-09-02 22:11:06 +0300

    fixes: env config can break the default config creation
    Default configuration files are not created when users explicitly set env
    variables to the default paths. Server initializing step did not check if user
    provided path is also same as default config path.

--- dist/container/server_init.sh
+++ dist/container/server_init.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
-CONFIG_TEMPLATE="/usr/local/share/nut_webgui/config.toml"
+TEMPLATE_CONFIG_FILE="/usr/local/share/nut_webgui/config.toml"
+TEMPLATE_USERS_FILE="/usr/local/share/nut_webgui/users.toml"
 DEFAULT_CONFIG_DIR="/etc/nut_webgui"
 DEFAULT_ROOT_CA="/etc/ssl/certs/cert.pem"
 DEFAULT_SERVER_KEY="$DEFAULT_CONFIG_DIR/server.key"
-DEFAULT_CONFIG="$DEFAULT_CONFIG_DIR/config.toml"
+DEFAULT_CONFIG_FILE="$DEFAULT_CONFIG_DIR/config.toml"
+DEFAULT_USERS_FILE="$DEFAULT_CONFIG_DIR/users.toml"
 
 APP_GROUP="nut_webgui"
 APP_USER="nut_webgui"
@@ -52,15 +54,21 @@ if [ "$CURRENT_UID" -eq "0" ]; then
         install -d -m 775 -o "$APP_USER" -g "$APP_GROUP" "$DEFAULT_CONFIG_DIR"
     fi
 
-    if [ -z "$NUTWG__CONFIG_FILE" ]; then
-        if [ ! -e "$DEFAULT_CONFIG" ]; then
-            install -m 664 -o "$APP_USER" -g "$APP_GROUP" "$CONFIG_TEMPLATE" "$DEFAULT_CONFIG"
+    if [ -z "$NUTWG__CONFIG_FILE" -o "$NUTWG__CONFIG_FILE" = "$DEFAULT_CONFIG_FILE" ]; then
+        if [ ! -e "$DEFAULT_CONFIG_FILE" ]; then
+            install -m 664 -o "$APP_USER" -g "$APP_GROUP" "$TEMPLATE_CONFIG_FILE" "$DEFAULT_CONFIG_FILE"
         fi
 
-        NUTWG__CONFIG_FILE="$DEFAULT_CONFIG"
+        NUTWG__CONFIG_FILE="$DEFAULT_CONFIG_FILE"
     fi
 
-    if [ -z "$NUTWG__SERVER_KEY" ]; then
+    if [ -z "$NUTWG__AUTH__USERS_FILE" -o "$NUTWG__AUTH__USERS_FILE" = "$DEFAULT_USERS_FILE" ]; then
+        if [ ! -e "$DEFAULT_USERS_FILE" ]; then
+            install -m 664 -o "$APP_USER" -g "$APP_GROUP" "$TEMPLATE_USERS_FILE" "$DEFAULT_USERS_FILE"
+        fi
+    fi
+
+    if [ -z "$NUTWG__SERVER_KEY" -o "$NUTWG__SERVER_KEY" = "$DEFAULT_SERVER_KEY" ]; then
         if [ ! -e "$DEFAULT_SERVER_KEY" ]; then
             head -c 128 /dev/urandom | sha256sum -b | head -c 64 > "$DEFAULT_SERVER_KEY"
             chmod 640 "$DEFAULT_SERVER_KEY";
@@ -74,33 +82,28 @@ else
     echo "Skipping CA linking and no default config will be generated on /etc/nut_webgui."
 fi
 
-EXEC="$(cat << EOF
-    export NUTWG__AUTH__ALLOW_ANONYMOUS_METRICS="$NUTWG__AUTH__ALLOW_ANONYMOUS_METRICS";
-    export NUTWG__AUTH__USERS_FILE="$NUTWG__AUTH__USERS_FILE";
-    export NUTWG__CONFIG_FILE="$NUTWG__CONFIG_FILE";
-    export NUTWG__DEFAULT_THEME="$NUTWG__DEFAULT_THEME";
-    export NUTWG__HTTP_SERVER__BASE_PATH="$NUTWG__HTTP_SERVER__BASE_PATH";
-    export NUTWG__HTTP_SERVER__LISTEN="$NUTWG__HTTP_SERVER__LISTEN";
-    export NUTWG__HTTP_SERVER__PORT="$NUTWG__HTTP_SERVER__PORT";
-    export NUTWG__HTTP_SERVER__WORKER_COUNT="$NUTWG__HTTP_SERVER__WORKER_COUNT";
-    export NUTWG__LOG_LEVEL="$NUTWG__LOG_LEVEL";
-    export NUTWG__SERVER_KEY="$NUTWG__SERVER_KEY";
-    export NUTWG__UPSD__ADDRESS="$NUTWG__UPSD__ADDRESS";
-    export NUTWG__UPSD__MAX_CONNECTION="$NUTWG__UPSD__MAX_CONNECTION";
-    export NUTWG__UPSD__NAME="$NUTWG__UPSD__NAME";
-    export NUTWG__UPSD__PASSWORD="$NUTWG__UPSD__PASSWORD";
-    export NUTWG__UPSD__POLL_FREQ="$NUTWG__UPSD__POLL_FREQ";
-    export NUTWG__UPSD__POLL_INTERVAL="$NUTWG__UPSD__POLL_INTERVAL";
-    export NUTWG__UPSD__PORT="$NUTWG__UPSD__PORT";
-    export NUTWG__UPSD__TLS_MODE="$NUTWG__UPSD__TLS_MODE";
-    export NUTWG__UPSD__USERNAME="$NUTWG__UPSD__USERNAME";
-
-    exec $@
-EOF
-)"
+export NUTWG__CONFIG_FILE
+export NUTWG__DEFAULT_THEME
+export NUTWG__LOG_LEVEL
+export NUTWG__SERVER_KEY
+export NUTWG__AUTH__ALLOW_ANONYMOUS_METRICS
+export NUTWG__AUTH__USERS_FILE
+export NUTWG__HTTP_SERVER__BASE_PATH
+export NUTWG__HTTP_SERVER__LISTEN
+export NUTWG__HTTP_SERVER__PORT
+export NUTWG__HTTP_SERVER__WORKER_COUNT
+export NUTWG__UPSD__ADDRESS
+export NUTWG__UPSD__MAX_CONNECTION
+export NUTWG__UPSD__NAME
+export NUTWG__UPSD__PASSWORD
+export NUTWG__UPSD__POLL_FREQ
+export NUTWG__UPSD__POLL_INTERVAL
+export NUTWG__UPSD__PORT
+export NUTWG__UPSD__TLS_MODE
+export NUTWG__UPSD__USERNAME
 
 if [ "$CURRENT_UID" -eq "0" ]; then
-    exec su -s "/bin/sh" -c "$EXEC" "$APP_USER"
+    exec su -p -s "/bin/sh" -c 'exec "$@"' -- "$APP_USER" sh "$@"
 else
-    exec sh -c "$EXEC"
+    exec "$@"
 fi
